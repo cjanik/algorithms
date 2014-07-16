fc03788e2a16908b20809d57f29f7d43f9ad9f62
Aaron Rice <adrice727@gmail.com>
2014-07-16 13:26:10 -0700

    powerSet

---
+++ powerSet.js
@@ -0,0 +1,22 @@
+
+var powerSet = function(string) {
+
+  var set = [];
+
+  var buildSet = function(base, rest) {
+
+    if ( rest.length === 0 ) { return; }
+
+    for ( var i = 0; i < rest.length; i++ ) {
+      var newBase = base + rest.charAt(i);
+      set.push(newBase);
+      buildSet(newBase, rest.slice(i+1));
+    }
+  }
+
+  buildSet('', string);
+  
+  return set;
+};
+
+console.log(powerSet('abcd'));
