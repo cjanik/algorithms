c6deba011124c22414e8e4bfada32d0ca656904d
Aaron Rice <adrice727@gmail.com>
2014-07-23 16:17:38 -0700

    createBSTFromList

--- createBSTFromList.js
+++ createBSTFromList.js
@@ -1,6 +1,8 @@
-// Given an array of integers sorted in ascending order, create a BST with minimal height
+/*
+// Given a sorted array of integers, create a BST with minimal height.
+*/
 
-var createBSTFromList = function(array) {
+var bstFromList = function(array) {
 
   if ( array.length === 0 ) { return null; }
 
@@ -13,10 +15,10 @@ var createBSTFromList = function(array) {
   }
 
   var left = array.slice(0, midIndex);
-  var right = array.slice(midIndex);
+  var right = array.slice(midIndex + 1);
 
-  tree.left = createBSTFromList(left);
-  tree.right = createBSTFromList(right);
+  tree.left = bstFromList(left);
+  tree.right = bstFromList(right);
 
   return tree;
 
@@ -25,12 +27,48 @@ var createBSTFromList = function(array) {
 
 var treeNode = function(value) {
   
-  var node = {};
-  node.value = value;
-  node.left = null;
-  node.right = null;
+  this.value = value;
+  this.left = null;
+  this.right = null;
 
-  return node;
 };
 
+treeNode.prototype.inOrderTraversal = function(nodeList, parent) {
+  
+  nodeList = nodeList || [];
+  this.parent = this.parent || parent; // This will be undefined for the root node
+
+  this.leftChildTraversed = false;
+  this.rightChildTraversed = false;
+
+  // If there is a left child and it hasn't been traversed
+  if ( this.left && !this.leftChildTraversed ) {
+    this.left.inOrderTraversal(nodeList, this);
+    this.leftChildTraversed = true;
+  }
+
+  // Add the current node to the nodeList
+  if ( !this.left || this.leftChildTraversed ) {
+    nodeList.push(this.value);
+    this.addedToList = true;
+  }
+
+  // If there is a right child and it hasn't been traversed
+  if ( this.right && !this.rightChildTraversed ) {
+    this.right.inOrderTraversal(nodeList, this);
+    this.rightChildTraversed = true;
+  }
+
+  // Once the right child has been traversed, go back up the tree, if possible
+  if ( !this.right  || this.rightChildTraversed ) {
+    if ( this.parent ) {
+      return;
+    } else {
+      return nodeList;
+    }
+  }
+}
+
 var testArray = [2, 4, 5, 6, 9, 10, 13, 23, 34, 45, 60];
+var testTree = bstFromList(testArray);
+testTree.inOrderTraversal();
